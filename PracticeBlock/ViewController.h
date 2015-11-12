515bd7b8ed15bc753abc63074e63cfdb8ba49f35
rocooshiang <rocoo.shaing@gmail.com>
2015-11-12 10:48:32 +0800

    Initial Commit

---
+++ PracticeBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeBlock
+//
+//  Created by Geosat-RD01 on 2015/11/12.
+//  Copyright © 2015年 Geosat-RD01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PracticeBlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PracticeBlock
+//
+//  Created by Geosat-RD01 on 2015/11/12.
+//  Copyright © 2015年 Geosat-RD01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
